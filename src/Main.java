603cf9d3e8bb2fe6a51254a32223e0ed6a3d6d49
liamebs <simon.ebsworth@yahoo.co.uk>
2023-04-24 17:55:56 +0100

    Full code comment update for Q5

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
-public class Main { // create a class attribute
+public class Main {
+    // create a class attribute 'x':
     int x;
     public Main() { // create a CLASS CONSTRUCTOR for the main class
-        x = 5; // set the initial value for the class attribute x
+        x = 5; // CONSTRUCTOR - set the initial value for the class attribute x
     }
 
     public static void main(String[] args) {
             Main myObj = new Main(); /*
             create an object of class Main;
-            this will CALL THE CONSTRUCTOR
+            this will CALL THE CONSTRUCTOR to instantiate myObj
+            with a property(attribute) called 'x'
+            with a value 5.
             */
-        System.out.println(myObj.x); // print the value of x
+        System.out.println(myObj.x);
+        /* print the value of x
+        as set in the constructor method
+        of the Main class
+         */
+
     }
 }
